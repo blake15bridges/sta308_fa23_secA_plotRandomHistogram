ede56c6c82b977ed01c69272287e191cd6d9f5b0
blake15bridges <bridgeb3@miamioh.edu>
2023-10-20 15:44:27 -0400

    Added source code for the plot random function, also updated readme

---
+++ plotRandomHistogram.R
@@ -0,0 +1,12 @@
+#* plotRandomHistogram.R
+#* 
+#* Author: Blake Bridges
+#* 
+#* Purpose:
+#*     A function that will generate 
+#*     a sample of Normal "observations"
+#*     and plot the histogram of that data.
+#*     
+#*
+
+     
